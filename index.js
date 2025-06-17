f88d887f228061255dd0dd62a14d1cc7b0909766
luxorAdesto <daviepaca@gmail.com>
2025-06-17 19:34:29 +0000

    6

--- index.js
+++ index.js
@@ -56,9 +56,6 @@ app.post('/check-verification', (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`FlameGate listening on port ${PORT}`);
 });
